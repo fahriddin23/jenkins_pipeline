0a845fff5083da19035c5179b0bf765f1248c162
fahriddin23 <fahriddin24@gmail.com>
2019-08-24 11:24:31 -0500

    done

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 node {
     stage("Say Hello"){
-        properties([parameters([string(defaultValue: 'Fahriddin', description: 'adds a name', name: 'NAME', trim: false)]), pipelineTriggers([cron('* * * * *')])])
+        properties([parameters([string(defaultValue: 'Dev', description: '''Dev: 18.219.171.79 QA: 18.219.242.93 Prod: 3.17.206.126''', name: 'Remote_instances', trim: false)])])
         git 'https://github.com/fahriddin23/jenkins.git'
     }
 }
